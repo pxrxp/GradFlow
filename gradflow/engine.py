e3fa05fcc8766d86bdbbc6bf537b55ae8b0cb20e
pxrxp <187556078+pxrxp@users.noreply.github.com>
2026-03-03 21:11:51 +0545

    Edit engine.py to add type annotations and clean up code

--- gradflow/engine.py
+++ gradflow/engine.py
@@ -1,15 +1,16 @@
 import math
+from typing import Set, Tuple, Union, Callable, List
 
 class Value:
-    def __init__(self, data, _parents=(), _op=''):
-        self.data = data
-        self.grad = 0
-        self._backward = lambda: None
-        self._parents = set(_parents)
-        self._op = _op
+    def __init__(self, data: float, _parents: Tuple["Value", ...] = (), _op: str = ""):
+        self.data: float = data
+        self.grad: float = 0
+        self._backward: Callable[[], None] = lambda: None
+        self._parents: Set["Value"] = set(_parents)
+        self._op: str = _op
 
     # self + other
-    def __add__(self, other):
+    def __add__(self, other: Union["Value", float]) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
         def _backward():
@@ -19,7 +20,7 @@ class Value:
         return out
 
     # self * other
-    def __mul__(self, other):
+    def __mul__(self, other: Union["Value", float]) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
         def _backward():
@@ -29,7 +30,7 @@ class Value:
         return out
 
     # self ** other
-    def __pow__(self, other):
+    def __pow__(self, other: Union[int, float]) -> "Value":
         assert isinstance(other, (int, float))
         out = Value(self.data**other, (self,), f'^{other}')
         def _backward():
@@ -37,17 +38,17 @@ class Value:
         out._backward = _backward
         return out
 
-    def relu(self):
+    def relu(self) -> 'Value':
         out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
         def _backward():
             self.grad += (out.data > 0) * out.grad
         out._backward = _backward
         return out
 
-    def backward(self):
-        topo = []
-        visited = set()
-        def build_topo(v):
+    def backward(self) -> None:
+        topo: List['Value'] = []
+        visited: Set['Value'] = set()
+        def build_topo(v: 'Value'):
             if v not in visited:
                 visited.add(v)
                 for parent in v._parents:
@@ -59,32 +60,32 @@ class Value:
             v._backward()
 
     # -self
-    def __neg__(self):
+    def __neg__(self) -> "Value":
         return self * -1
 
     # other + self
-    def __radd__(self, other):
+    def __radd__(self, other: Union["Value", float]) -> "Value":
         return self + other
 
     # self - other
-    def __sub__(self, other):
+    def __sub__(self, other: Union["Value", float]) -> "Value":
         return self + (-other)
 
     # other - self
-    def __rsub__(self, other):
+    def __rsub__(self, other: Union["Value", float]) -> "Value":
         return Value(other) + (-self)
 
     # other * self
-    def __rmul__(self, other):
+    def __rmul__(self, other: Union["Value", float]) -> "Value":
         return self * other
 
     # self / other
-    def __truediv__(self, other):
+    def __truediv__(self, other: Union["Value", float]) -> "Value":
         return self * other**-1
 
     # other / self
-    def __rtruediv__(self, other):
+    def __rtruediv__(self, other: Union["Value", float]) -> "Value":
         return Value(other) * self**-1
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Value(data={self.data}, grad={self.grad})"
